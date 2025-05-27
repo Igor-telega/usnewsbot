a37161ec366c367e9bcfc5a8b161a26fe5430ac4
Igor-telega <idavpost@gmail.com>
2025-05-27 08:58:10 -0700

    Update image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -1,10 +1,11 @@
 import openai
 
 def generate_image(prompt):
-    response = openai.Image.create(
+    response = openai.images.generate(
         model="dall-e-3",
         prompt=prompt,
         size="1024x1024",
+        quality="standard",
         n=1
     )
-    return response['data'][0]['url']
+    return response.data[0].url
